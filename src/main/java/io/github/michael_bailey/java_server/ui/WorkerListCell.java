5b5fb89116c07913e39edf880b031bbebce2308c
michael bailey <mickyb18a@gmail.com>
2020-06-10 18:11:38 +0100

    attempted to update ui with workers

--- src/main/java/io/github/michael_bailey/java_server/ui/WorkerListCell.java
+++ src/main/java/io/github/michael_bailey/java_server/ui/WorkerListCell.java
@@ -4,11 +4,14 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import io.github.michael_bailey.java_server.classes.Worker;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.PopupWindow;
 
 public class WorkerListCell extends ListCell<Worker> {
 
     @FXML public Label usernameLabel;
     @FXML public Label uuidLabel;
+    @FXML public AnchorPane root;
 
     @Override
     protected void updateItem(Worker item, boolean empty) {
@@ -18,5 +21,7 @@ public class WorkerListCell extends ListCell<Worker> {
             this.setText("Error NullPointer");
             this.setGraphic(null);
         }
+
+        this.setGraphic(root);
     }
 }
