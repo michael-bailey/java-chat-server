59fd724023c0a05cab93306c5aea452665945631
michael bailey <mickyb18a@gmail.com>
2020-06-10 17:56:30 +0100

    Update Command.java
    - reomved debug messages

--- src/main/java/io/github/michael_bailey/java_server/Protocol/Command.java
+++ src/main/java/io/github/michael_bailey/java_server/Protocol/Command.java
@@ -46,13 +46,8 @@ public class Command {
     }
 
     public static Command valueOf(String string) {
-        System.out.println("string = " + string);
         Matcher matcher = parser.matcher(string);
-
-        System.out.println("matcher find = " + matcher.find());
-
         String command = matcher.group();
-        System.out.println("command = " + command);
 
         HashMap<String, String> params = new HashMap<>();
         while (matcher.find()) {
